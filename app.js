3e4c58e3df3933c60578f95f8c7720a0ac177e97
aaronshivers <aaron@moonmail.org>
2019-10-17 12:51:28 -0500

    added morgan for logging

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 require('dotenv').config()
 
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const environment = process.env.NODE_ENV
 
 const locationsRoutes = require('./routes/locations')
+
+if (environment === 'development') app.use(morgan('dev'))
 
 app.use('/api/v1/locations', locationsRoutes)
 
 app.listen(
   PORT, () => {
-    console.log(`Server running in ${ process.env.NODE_ENV } mode on port ${ PORT }`)
+    console.log(`Server running in ${ environment } mode on port ${ PORT }`)
   }
 )
---
+++ middleware/logger.js
@@ -0,0 +1,5 @@
+// @desc    logs request to console
+module.exports = (req, res, next) => {
+  console.log(`${ req.method } ${ req.protocol }://${ req.get('host') }${ req.originalUrl }`)
+  next()
+}
